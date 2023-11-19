1f2b4e60d029e6d0aeeaea6dab0757aa35a5e68e
tison <wander4096@gmail.com>
2023-11-19 09:17:44 +0800

    feat: impl Generation
    Signed-off-by: tison <wander4096@gmail.com>

--- src/server/src/statemachine/mod.rs
+++ src/server/src/statemachine/mod.rs
@@ -14,7 +14,7 @@
 
 use bytes::BufMut;
 
-#[derive(Default, Debug, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Default, Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
 pub struct Revision {
     main: u64,
     sub: u64,
@@ -34,5 +34,120 @@ impl Revision {
             bs.put_u8(b't');
         }
         bs
+    }
+}
+
+#[derive(Debug)]
+pub struct Generation {
+    version: u64,
+    created: Revision,
+    revisions: Vec<Revision>,
+}
+
+impl Generation {
+    pub fn new(version: u64, created: Revision, revisions: Vec<Revision>) -> Generation {
+        Generation {
+            version,
+            created,
+            revisions,
+        }
+    }
+
+    pub fn get_revision(&self, n: usize) -> Revision {
+        self.revisions[n]
+    }
+
+    pub fn get_first_version(&self) -> Revision {
+        self.revisions[0]
+    }
+
+    pub fn get_last_revision(&self) -> Revision {
+        self.revisions[self.revisions.len() - 1]
+    }
+
+    pub fn add_revision(&mut self, revision: Revision) {
+        self.revisions.push(revision);
+        self.version += 1;
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.revisions.is_empty()
+    }
+
+    pub fn walk(&self, predicate: impl Fn(Revision) -> bool) -> Option<usize> {
+        let len = self.revisions.len();
+        for i in 0..len {
+            let idx = len - i - 1;
+            if !predicate(self.revisions[idx]) {
+                return Some(idx);
+            }
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::statemachine::{Generation, Revision};
+
+    #[test]
+    fn test_revisions() {
+        let revisions = vec![
+            Revision::default(),
+            Revision::new(1, 0),
+            Revision::new(1, 1),
+            Revision::new(2, 0),
+            Revision::new(u64::MAX, u64::MAX),
+        ];
+
+        for i in 0..revisions.len() - 1 {
+            assert!(revisions[i] < revisions[i + 1])
+        }
+    }
+
+    #[test]
+    fn test_generation_walk() {
+        let revisions = vec![
+            Revision::new(2, 0),
+            Revision::new(4, 0),
+            Revision::new(6, 0),
+        ];
+        let generation = Generation::new(3, revisions[0], revisions);
+
+        struct TestCase {
+            predicate: fn(Revision) -> bool,
+            result: Option<usize>,
+        }
+
+        let cases = vec![
+            TestCase {
+                predicate: |r| r.main >= 7,
+                result: Some(2),
+            },
+            TestCase {
+                predicate: |r| r.main >= 6,
+                result: Some(1),
+            },
+            TestCase {
+                predicate: |r| r.main >= 5,
+                result: Some(1),
+            },
+            TestCase {
+                predicate: |r| r.main >= 4,
+                result: Some(0),
+            },
+            TestCase {
+                predicate: |r| r.main >= 3,
+                result: Some(0),
+            },
+            TestCase {
+                predicate: |r| r.main >= 2,
+                result: None,
+            },
+        ];
+
+        for case in cases {
+            assert_eq!(generation.walk(case.predicate), case.result);
+        }
     }
 }
